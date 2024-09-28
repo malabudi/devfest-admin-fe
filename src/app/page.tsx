25f5b554cbc61fb4e61382c389058413f141f168
Abhijay Das <abhijayd@umich.edu>
2024-09-28 17:23:14 -0400

    builds the login ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center p-64">
       <main className="flex flex-col gap-8 items-center">
-        <h1 className="text-center text-4xl">DevFest Admin Login</h1>
-        {/*Remove Later*/}
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ante nisl, ultrices non blandit pretium, facilisis sagittis arcu. Aenean sollicitudin convallis porta. Aenean pulvinar augue ut dapibus scelerisque. Quisque vehicula nunc nec neque auctor ultrices. Curabitur eleifend gravida pellentesque. Cras maximus metus velit, eu volutpat diam interdum nec. Morbi tincidunt mauris a ante pretium, non vestibulum arcu dignissim. Vestibulum scelerisque efficitur lacinia. Fusce maximus porta arcu quis consectetur. Nunc non odio a magna faucibus semper. Praesent pulvinar, nulla vel tincidunt porttitor, massa erat varius est, ut molestie massa nisl sit amet mauris. Mauris at lobortis magna. Aliquam rutrum odio ut volutpat placerat. Mauris iaculis urna luctus, sodales enim ut, eleifend mi. Cras malesuada quam eu justo fermentum ultrices. </p>
+        <h1 className="text-center text-6xl">MI DevFest Admin</h1>
+        <form className="flex flex-col">
+          <label>Username*</label>
+          <input className="w-96 mb-5 border border-solid border-gray-200 rounded py-1 pl-2" id="username" name="username"></input>
+          <label>Password*</label>
+          <input className="w-96 mb-12 border border-solid border-gray-200 rounded py-1 pl-2" id="password" name="password"></input>
+          <button className="bg-green-500 text-white w-96 rounded py-1" type="submit">Log In</button>
+        </form>
       </main>
     </div>
   );
