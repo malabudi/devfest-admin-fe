9093bd734836ca30f158290573c8f0d75b5fc139
kllarena07 <baymax709@gmail.com>
2024-10-02 19:25:28 -0400

    fix: display table border

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,16 +3,15 @@ import { ReactNode } from 'react';
 
 export default function Table({ rows, columns, editRow, deleteRow }: { rows: any[], columns: string[], editRow: () => void, deleteRow: (index: number) => void }) {
     const MAX_CHARACTERS = 13;
-    const BORDER_WIDTH = 2;
 
     return (
         <table className="font-bold border-b-8 w-full">
             <thead className='text-left'>
                 <tr>
                     {columns.map((column) => (
-                        <th className={`border-r-${BORDER_WIDTH} border-b-${BORDER_WIDTH} p-4`} key={column}>{column}</th>
+                        <th className="border-r-2 border-b-2 p-4" key={column}>{column}</th>
                     ))}
-                    <th className={`border-b-${BORDER_WIDTH} p-4`}></th>
+                    <th className="border-b-2 p-4"></th>
                 </tr>
             </thead>
             <tbody>
@@ -20,12 +19,12 @@ export default function Table({ rows, columns, editRow, deleteRow }: { rows: any
                     <tr key={row.name}>
                         {columns.map((column, index) => {
                             const vals_array: ReactNode[] = Array.from(Object.values(row));
-                            return <td className={`max-w-full border-r-${BORDER_WIDTH} border-b-${BORDER_WIDTH} p-5`} key={column}>{
+                            return <td className="max-w-full border-r-2 border-b-2 p-5" key={column}>{
                                 typeof vals_array[index] === 'string' && vals_array[index].length > MAX_CHARACTERS ? vals_array[index].substring(0, MAX_CHARACTERS) + '...' : vals_array[index]
                                 }
                             </td>
                             })}
-                        <td className={`border-b-${BORDER_WIDTH}`}>
+                        <td className="border-b-2">
                             <div className="grid grid-cols-2 p-5">
                                 <button className='flex justify-start' onClick={editRow}>
                                     <Image src="/pencil.svg" alt="Edit row." width={20} height={20} />
