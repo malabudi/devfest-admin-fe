fdd5d0ec008536680919ea5642e6b282ab3f96ff
kllarena07 <baymax709@gmail.com>
2024-10-01 20:31:41 -0400

    feat: skeleton edit and delete func demo

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { ReactNode } from 'react';
 
-export default function Table({ rows, columns }: { rows: any[], columns: string[] }) {
+export default function Table({ rows, columns, editRow, deleteRow }: { rows: any[], columns: string[], editRow: () => void, deleteRow: (index: number) => void }) {
     const MAX_CHARACTERS = 13;
     const BORDER_WIDTH = 2;
 
@@ -16,21 +16,23 @@ export default function Table({ rows, columns }: { rows: any[], columns: string[
                 </tr>
             </thead>
             <tbody>
-                {rows.map((row) => (
-                    <tr key={row.id}>
-                        {columns.map((_, index) => {
+                {rows.map((row, index) => (
+                    <tr key={row.name}>
+                        {columns.map((column, index) => {
                             const vals_array: ReactNode[] = Array.from(Object.values(row));
-                            return <td className={`max-w-full border-r-${BORDER_WIDTH} border-b-${BORDER_WIDTH} p-5`}>{
+                            return <td className={`max-w-full border-r-${BORDER_WIDTH} border-b-${BORDER_WIDTH} p-5`} key={column}>{
                                 typeof vals_array[index] === 'string' && vals_array[index].length > MAX_CHARACTERS ? vals_array[index].substring(0, MAX_CHARACTERS) + '...' : vals_array[index]
                                 }
                             </td>
                             })}
                         <td className={`border-b-${BORDER_WIDTH}`}>
                             <div className="grid grid-cols-2 p-5">
-                                <button className='flex justify-start'>
+                                <button className='flex justify-start' onClick={editRow}>
                                     <Image src="/pencil.svg" alt="Edit row." width={20} height={20} />
                                 </button>
-                                <button className='flex justify-end'>
+                                <button className='flex justify-end' onClick={() => {
+                                    deleteRow(index);
+                                }}>
                                     <Image src="/trash.svg" alt="Delete row." width={20} height={20} />
                                 </button>
                             </div>
