e48836ca18c49f18ddb420189fc34179f5d5c62c
kllarena07 <baymax709@gmail.com>
2024-10-01 20:04:57 -0400

    feat: table component

--- src/app/organizers/page.tsx
+++ src/app/organizers/page.tsx
@@ -1,7 +1,15 @@
+import Table from "@/components/Table";
+
 export default function Home() {
+    const columns = ["Name", "LinkedIn", "GitHub", "Avatar", "Bio", "University"];
+    const rows = [
+      { name: "John Doe", email: "john@gmail.com", linkedin: "linkedin.com/johndoe", github: "github.com/johndoe", avatar: "https://via.placeholder.com/150", bio: "I am a software engineer.", university: "University of Toronto" },
+    ];
+
     return (
-      <div className="min-h-screen flex items-center justify-center p-64">
+      <div className="min-h-screen flex flex-col items-center justify-center p-64">
           <h1 className="text-center text-4xl">Organizers Table</h1>
+          <Table rows={rows} columns={columns}  />
       </div>
     );
   }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,43 @@
-export default function Table() {
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+export default function Table({ rows, columns }: { rows: any[], columns: string[] }) {
+    const MAX_CHARACTERS = 13;
+    const BORDER_WIDTH = 2;
+
     return (
-        <div>
-            <p1>Create table component here</p1>
-        </div>
+        <table className="font-bold border-b-8 w-full">
+            <thead className='text-left'>
+                <tr>
+                    {columns.map((column) => (
+                        <th className={`border-r-${BORDER_WIDTH} border-b-${BORDER_WIDTH} p-4`} key={column}>{column}</th>
+                    ))}
+                    <th className={`border-b-${BORDER_WIDTH} p-4`}></th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map((row) => (
+                    <tr key={row.id}>
+                        {columns.map((_, index) => {
+                            const vals_array: ReactNode[] = Array.from(Object.values(row));
+                            return <td className={`max-w-full border-r-${BORDER_WIDTH} border-b-${BORDER_WIDTH} p-5`}>{
+                                typeof vals_array[index] === 'string' && vals_array[index].length > MAX_CHARACTERS ? vals_array[index].substring(0, MAX_CHARACTERS) + '...' : vals_array[index]
+                                }
+                            </td>
+                            })}
+                        <td className={`border-b-${BORDER_WIDTH}`}>
+                            <div className="grid grid-cols-2 p-5">
+                                <button className='flex justify-start'>
+                                    <Image src="/pencil.svg" alt="Edit row." width={20} height={20} />
+                                </button>
+                                <button className='flex justify-end'>
+                                    <Image src="/trash.svg" alt="Delete row." width={20} height={20} />
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
