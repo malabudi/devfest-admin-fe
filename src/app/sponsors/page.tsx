d62716b36ffa7b14a6ab29c514d0feacff22ae85
sohank1 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-10-24 10:29:08 -0400

    render data from http request

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -1,7 +1,33 @@
+'use client';
+
+import Table from "@/components/Table";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 export default function Home() {
-    return (
-      <div className="min-h-screen flex items-center justify-center p-64">
-          <h1 className="text-center text-4xl">Sponsors Table</h1>
-      </div>
-    );
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('http://localhost:5000/sponsors').then((res) =>
+        res.json(),
+      ),
+  })
+
+  const editRow = () => {
+    // Row editing logic here
+    console.log("Edit row.");
   }
+
+  console.log("etste", data)
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-64">
+      <h1 className="text-center text-4xl">Sponsors Table</h1>
+      <Table rows={data ? data
+
+        : []} columns={["Name", "Description", "Year", "URL"]} deleteRow={editRow} editRow={editRow} />
+
+    </div>
+  );
+}
